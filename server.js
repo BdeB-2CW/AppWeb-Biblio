c5025760880cd55e97fd508447cfe3539ebb7183
czbstar <63319380+czbstar@users.noreply.github.com>
2022-02-08 23:55:34 +0800

    server.js
    creation de server  au port localhost 4000 et les pages

---
+++ server.js
@@ -0,0 +1,70 @@
+// Ce document est écrit 99% par Zhi Bo Cao
+
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.static('public'))
+app.use('/CSS', express.static(__dirname + 'CSS'))
+
+app.set('view engine', 'ejs');
+
+
+
+
+/**
+ * Pages d'ouverture
+ */
+
+app.get('/login', async (req, res) => {
+    //show all users
+    //const users = await User.find()
+    //console.log(users)
+    res.render('login');
+});
+
+//register and login
+app.post('/login', async (req, res) => {
+    const dataReceived = req.body
+    //console.log(dataReceived);
+
+    //register
+ 
+
+    //login
+
+    res.redirect("/")
+        //end of login
+    
+
+ 
+});//end of post
+
+//acceuil
+app.get('/',(req,res) => {
+    res.render('Acceuil');
+});
+
+//recherche liste de donnee
+app.get('/recherche', (req, res) => {  
+    res.render('recherche')  
+})
+
+//livre
+app.get('/livres/:livre', (req, res) => {
+    res.render('livres')
+});
+
+//profil
+app.get('/profils/:profil', (req, res) => {
+    res.render('profil')
+
+});
+
+
+app.listen(port, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", port);
+});
+
+
